8bf7e35ea0c7000a0810c603008a745b50cdb773
Kamil Ochał <kamilochal@MacBook-Air-Kamil.local>
2023-02-22 08:42:43 +0100

    ios setup update and PAYU API fix

--- lib/utils/json_parser.dart
+++ lib/utils/json_parser.dart
@@ -5,27 +5,25 @@ import 'package:kiosk_flutter/models/storage_limits_model.dart';
 import 'package:kiosk_flutter/models/storage_model.dart';
 
 class JsonParser {
+  List<StorageModel> parseStorage(String json) => jsonDecode(json)
+      .cast<Map<String, dynamic>>()
+      .map<StorageModel>((x) => StorageModel.fromJson(x))
+      .toList();
 
-  List<StorageModel> parseStorage(String json) =>
-      jsonDecode(json)
-        .cast<Map<String, dynamic>>()
-        .map<StorageModel>((x) => StorageModel.fromJson(x))
-        .toList();
+  List<StorageLimitsModel> parseStorageLimits(String json) => jsonDecode(json)
+      .cast<Map<String, dynamic>>()
+      .map<StorageLimitsModel>((x) => StorageLimitsModel.fromJson(x))
+      .toList();
 
-  List<StorageLimitsModel> parseStorageLimits(String json) =>
-      jsonDecode(json)
-        .cast<Map<String, dynamic>>()
-        .map<StorageLimitsModel>((x) => StorageLimitsModel.fromJson(x))
-        .toList();
-
-  List<ContainerModel> parseContainers(String json) =>
-      jsonDecode(json)
-        .cast<Map<String, dynamic>>()
-        .map<ContainerModel>((x) => ContainerModel.fromJson(x))
-        .toList();
+  List<ContainerModel> parseContainers(String json) => jsonDecode(json)
+      .cast<Map<String, dynamic>>()
+      .map<ContainerModel>((x) => ContainerModel.fromJson(x))
+      .toList();
 
   List<PayMethodsModel> parsePayMethods(String json) {
+    print(jsonDecode(json)["pay_methods"]["payByLinks"].toString());
     final int id = jsonDecode(json)["id"] as int;
+
     return jsonDecode(json)["pay_methods"]["payByLinks"]
         .cast<Map<String, dynamic>>()
         .map<PayMethodsModel>((x) => PayMethodsModel.fromJson(x, id))
