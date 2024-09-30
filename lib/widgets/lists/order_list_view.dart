5415d0af15778d7bc95dae2a8e597b7d1c3686a9
Sebastian <sebastian.krajna@pomagier.info>
2024-09-30 15:12:56 +0200

    One more file

--- lib/widgets/lists/order_list_view.dart
+++ lib/widgets/lists/order_list_view.dart
@@ -1,25 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:kiosk_flutter/models/menu_item_with_description.dart';
 
 import 'package:kiosk_flutter/widgets/rows/list_rows/big_screen_order_list_row.dart';
 import 'package:kiosk_flutter/widgets/rows/list_rows/small_screnn_order_list_row.dart';
 
 class OrderList extends StatelessWidget {
-  final List<ProductTranslated> products;
+  final List<MenuItemWithDescription> items;
 
   const OrderList({
     super.key,
-    required this.products,
+    required this.items,
   });
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: products.length,
+      itemCount: items.length,
       itemBuilder: (context, index) {
         if (MediaQuery.of(context).size.height > 1000) {
-          return BigScreenOrderListRow(item: products[index]);
+          return BigScreenOrderListRow(item: items[index]);
         } else {
-          return SmallScreenOrderListRow(item: products[index]);
+          return SmallScreenOrderListRow(item: items[index]);
         }
       },
     );
